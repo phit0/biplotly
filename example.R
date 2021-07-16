0b731dd796c7ec9caf3df0f8e678dcc6c7f1574f
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-07-17 01:15:54 +0200

    second example

--- example.R
+++ example.R
@@ -4,3 +4,14 @@ data("iris")
 head(iris)
 
 biplotly(iris, color = "Species", components = c(1, 2), arr.scale = 0.02)
+
+
+data("mtcars")
+head(mtcars)
+
+mtcars$company <- rownames(mtcars) %>% 
+  stringr::str_split(" ") %>% 
+  lapply(function(x) {x[1]})
+
+debug(biplotly)
+biplotly(mtcars, color = "company", components = c(1, 2), arr.scale = 0.02)
