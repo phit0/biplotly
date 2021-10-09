449ed4d3becb1ab9fbc1aa826cefa07b1ed494ba
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-10-09 14:59:38 +0200

    [data] reduced big5 dataset

--- R/data.R
+++ R/data.R
@@ -12,14 +12,6 @@
 #'
 #' @format A tibble with 19719 rows and 57 variables:
 #' \describe{
-#'   \item{race}{Chosen from a drop down menu. 1=Mixed Race, 2=Arctic (Siberian, Eskimo),
-#'    3=Caucasian (European), 4=Caucasian (Indian), 5=Caucasian (Middle East),
-#'     6=Caucasian (North African, Other), 7=Indigenous Australian, 8=Native American,
-#'      9=North East Asian (Mongol, Tibetan, Korean Japanese, etc),
-#'       10=Pacific (Polynesian, Micronesian, etc),
-#'        11=South East Asian (Chinese, Thai, Malay, Filipino, etc),
-#'         12=West African, Bushmen, Ethiopian, 13=Other (0=missed)}
-#'   \item{age}{entered as text (individuals reporting age < 13 were not recorded)}
 #'   \item{engat}{Response to "is English your native language?". 1=yes, 2=no (0=missed)}
 #'   \item{gender}{gender	Chosen from a drop down menu. 1=Male, 2=Female, 3=Other (0=missed)}
 #'   \item{hand}{"What hand do you use to write with?". 1=Right, 2=Left, 3=Both (0=missed)}
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,5 +8,20 @@ codes <- scan("data-raw/codebook.txt",
   lapply(function(x){unlist(stringr::str_split(x, "\\t"))[2]})
 
 colnames(big5)[8:ncol(big5)] <- codes
+big5 <- dplyr::select(big5, !c("race","age"))
+big5$gender[which(big5$gender == 0)] <- "missed"
+big5$gender[which(big5$gender == 1)] <- "male"
+big5$gender[which(big5$gender == 2)] <- "female"
+big5$gender[which(big5$gender == 3)] <- "other"
 
+big5$engnat[which(big5$engnat == 0)] <- "missed"
+big5$engnat[which(big5$engnat == 1)] <- "yes"
+big5$engnat[which(big5$engnat == 2)] <- "no"
+
+big5$hand[which(big5$hand == 0)] <- "missed"
+big5$hand[which(big5$hand == 1)] <- "Right"
+big5$hand[which(big5$hand == 2)] <- "Left"
+big5$hand[which(big5$hand == 3)] <- "Both"
+
+big5$source <- as.factor(big5$source)
 usethis::use_data(big5, overwrite = TRUE, compress = T)
